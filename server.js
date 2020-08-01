85bdd68a983b3ac2d06a4085e13a664edb3846fc
Marina Mikhailik <marynapalkhouskaya@gmail.com>
2020-08-01 12:09:46 +0300

    'p'

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const server = http.createServer(function(request, response){
     console.log(request.method, request.url);
     response.end("Hello world!")
 });
-console.log('port = ', process.env.port );
-server.listen(process.env.port || 3032);
+console.log('port = ', process.env.PORT );
+server.listen(process.env.PORT || 3032);
 console.log("Servre is started!");
