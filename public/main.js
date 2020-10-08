6b747781a80ca6ec2ba4599bff600353434dfb97
Marina Mikhailik <marynapalkhouskaya@gmail.com>
2020-10-08 16:00:03 +0300

    use express

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
-const arrMenu = ["Account", "About game"];
+/* const arrMenu = ["Account", "About game"];
 
 arrMenu.forEach((menu)=>{
    let elm =  document.createElement("ul");
     elm.innerHTML = menu;
     document.body.append(elm);
-});
+});*/
