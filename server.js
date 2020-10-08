d66a337666e82b9070023ca6e0f3feb4adfc3191
Marina Mikhailik <marynapalkhouskaya@gmail.com>
2020-10-08 12:55:35 +0300

    use express

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 
+const port = process.env.PORT || 3032;
+
+app.use(express.static('public'));
+app.listen(port, () => {
+    console.log(`Aliac app is listening at port =${port}`);
+})
+
+
+/*
 const server = http.createServer(function(request, response){
     console.log(request.method, request.url);
-    response.end("Hello world!")
+  /* response.end('./index.html')
+    response.sendfile('index.html');
 });
+
 console.log('port = ', process.env.PORT );
 server.listen(process.env.PORT || 3032);
-console.log("Servre is started!");
+console.log("Server is started!");*/
